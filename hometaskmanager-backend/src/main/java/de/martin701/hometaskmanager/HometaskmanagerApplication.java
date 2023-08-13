eea1fca9258a1b36972e76b38eefd5f6b01120c0
Martin <martinnike88@gmail.com>
2023-08-13 20:48:53 +0200

    backend project created

---
+++ hometaskmanager-backend/src/main/java/de/martin701/hometaskmanager/HometaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package de.martin701.hometaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometaskmanagerApplication.class, args);
+	}
+
+}
---
+++ hometaskmanager-backend/src/test/java/de/martin701/hometaskmanager/HometaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.martin701.hometaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
